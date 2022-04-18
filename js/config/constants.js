795fdc77d35fd3a280cc4ca3879c4575f18e1890
GalcaVlad <vladgalca@gmail.com>
2022-04-18 16:50:29 +0300

    Prepared the ground for a rework

--- js/config/constants.js
+++ js/config/constants.js
@@ -5,48 +5,6 @@ export const COLOR_2 = "#346856";
 export const COLOR_3 = "#88c070";
 export const COLOR_4 = "#e0f8d0";
 
-// TILE
-export const TILE_WIDTH = 64;
-export const TILE_HEIGHT = 64;
-
-// MAP
-export const MAP_WIDTH = 5;
-export const MAP_HEIGHT = 3;
-
-// ROOMS
-export const ROOM_SIZE = 5;
-
-// TUNNELS
-export const MIN_TUNNEL_LENGTH = 2;
-export const MAX_TUNNEL_LENGTH = 3;
-
-export const MIN_TUNNEL_COUNT = 4;
-export const MAX_TUNNEL_COUNT = 6;
-
-// PLAYER
-export const PLAYER_HEALTH = 3;
-export const PLAYER_ATTACK_DAMAGE = 1;
-
-// ENTITIES
-export const MIN_ENEMY_COUNT = 1;
-export const MAX_ENEMY_COUNT = 1;
-
-// LEVELS
-const tilePath = "../../img/tiles/";
-const entityPath = "../../img/entities/";
-
-export const LEVELS = [
-    {
-        name: "Forest",
-        end: 2,
-        tiles: [
-            tilePath + "grass.png",
-            tilePath + "tree.png",
-            tilePath + "cave"
-        ],
-        entities: [
-            entityPath + "slime.png",
-            entityPath + "skeleton.png"
-        ]
-    }
-]
+// PATHS
+const TILE_PATH = "../../img/tiles/";
+const ENTITY_PATH = "../../img/entities/";
--- js/index.js
+++ js/index.js
@@ -1,18 +1,10 @@
 import * as constants from "./config/constants.js"
-import { Map } from "./game-map.js";
-import { FramesCounter } from "./interface/frames-counter.js";
 
 const cnv = document.querySelector("canvas");
 const ctx = cnv.getContext("2d");
 
 cnv.width = innerWidth;
 cnv.height = innerHeight;
-
-// Map
-var gameMap = new Map();
-
-// Utility
-var framesCounter = new FramesCounter(ctx, 10, 20, constants.COLOR_4);
 
 // Initialization
 window.onload = function () {
@@ -27,8 +19,5 @@ window.onload = function () {
 function drawGame() {
     if(ctx == null) return;
 
-    gameMap.draw(ctx);
-
-    // framesCounter.draw();
     requestAnimationFrame(drawGame);
 }
