4704afbb79c71e2738494a890e9df1336131c89c
GalcaVlad <vladgalca@gmail.com>
2022-04-23 18:31:17 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 import * as general from "./constants/general.js"
-import { ENEMIES } from "./constants/enemies.js";
-import { ALLIES } from "./constants/allies.js";
 import { backgroundPath, enemyPath } from "./utils/image_paths.js";
 
 const cnv = document.querySelector("canvas");
@@ -33,12 +31,6 @@ function animate() {
     var background = new Image();
     background.src = backgroundPath("forest");
     ctx.drawImage(background, 0, 0, cnv.width, cnv.height);
-
-    var enemy1 = new Image();
-    enemy1.src = enemyPath("evil_mage");
-    ctx.fillStyle = "crimson"
-    ctx.fillRect(cnv.width-304, cnv.height-160, 96, 32);
-    ctx.drawImage(enemy1, cnv.width-320, cnv.height-128-48, enemy1.width * 4, enemy1.height * 4);
 }
 
 function transition() {
