fdb732cf6556e2516a8b359a9c27031112859012
Iridynne <vladgalca@gmail.com>
2025-01-31 16:12:10 +0200

    Testing paths

--- js/utils/paths.js
+++ js/utils/paths.js
@@ -1,41 +1,41 @@
 // Image Paths
 export function allyPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/img/characters/allies/${name}.png`;
+    return `${window.location.href}/img/characters/allies/${name}.png`;
 }
 
 export function enemyPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/img/characters/enemies/${name}.png`;
+    return `${window.location.href}/img/characters/enemies/${name}.png`;
 }
 
 export function bossPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/img/characters/bosses/${name}.png`;
+    return `${window.location.href}/img/characters/bosses/${name}.png`;
 }
 
 export function backgroundPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/img/backgrounds/${name}.png`;
+    return `${window.location.href}/img/backgrounds/${name}.png`;
 }
 
 export function miscPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/img/misc/${name}.png`;
+    return `${window.location.href}/img/misc/${name}.png`;
 }
 
 export function projectilePath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/img/projectiles/${name}.png`;
+    return `${window.location.href}/img/projectiles/${name}.png`;
 }
 
 // Audio Paths
 export function soundPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/audio/sounds/${name}`;
+    return `${window.location.href}/audio/sounds/${name}`;
 }
 
 export function musicPath(name) {
     name = name.toLowerCase();
-    return `${window.location.origin}/audio/music/${name}`;
+    return `${window.location.href}/audio/music/${name}`;
 }
